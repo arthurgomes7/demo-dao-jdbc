67dbdb77d9fbbe277003bc6b980d444c5ff945bb
Arthur Gomes <arthurszaa7@icloud.com>
2026-01-20 23:26:41 -0300

    new: Criando Interfaces Dao

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    public void insert(Seller seller);
+    public void update(Seller seller);
+    public void deleteById(Integer id);
+    public Seller findById(Integer id);
+    public List<Seller> findAll();
+}
