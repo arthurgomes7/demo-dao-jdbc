b69f8301aca098e85ab59fe5ffdabae44d62d66e
Arthur Gomes <arthurszaa7@icloud.com>
2026-01-20 23:31:33 -0300

    new: Criando SellerDao e DaoFactory

--- src/model/entities/application/Program.java
+++ src/model/entities/application/Program.java
@@ -1,5 +1,7 @@
 package model.entities.application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         Seller seller = new Seller(2, "Arthur", "arthur@gmail.com", new Date(), 2300.00, department);
         System.out.println(seller);
