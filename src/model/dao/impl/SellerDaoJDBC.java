7a4f2637c682f6bfc662f4e441066b68260a5a88
Arthur Gomes <arthurszaa7@icloud.com>
2026-01-21 21:55:03 -0300

    new: Implementando funções do SellerDao

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,23 @@
 package model.dao.impl;
 
+import db.DB;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller seller) {
@@ -24,6 +36,41 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+                Department dp = new Department();
+                dp.setId(rs.getInt("DepartmentId"));
+                dp.setName(rs.getString("DepName"));
+
+                Seller seller = new Seller();
+                seller.setId(rs.getInt("Id"));
+                seller.setNome(rs.getString("Name"));
+                seller.setEmail(rs.getString("Email"));
+                seller.setSalario(rs.getDouble("BaseSalary"));
+                seller.setDataAniversario(rs.getDate("BirthDate"));
+                seller.setDepartamento(dp);
+                return seller;
+            }
+        }
+        catch (SQLException e){
+            e.getMessage();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
         return null;
     }
 
--- src/model/entities/application/Program.java
+++ src/model/entities/application/Program.java
@@ -9,12 +9,12 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        System.out.println(department);
+//        Department department = new Department(1, "Books");
+//        System.out.println(department);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        Seller seller = new Seller(2, "Arthur", "arthur@gmail.com", new Date(), 2300.00, department);
         System.out.println(seller);
     }
 }
